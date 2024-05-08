fac48f63a0c87e29f55658e3a75cadc22323451f
Jake <jsmith181997@gmail.com>
2024-05-07 21:55:12 -0400

    Hit weather app with latlong conversion

--- weather.js
+++ weather.js
@@ -1,8 +1,22 @@
 const key = '240bfc06d13fa4a6c479581cad040e61';
 const cityResultLimit = 5;
 
-function getWeather(cityName, stateCode, countryCode) {}
+// Main call to get weather.
+async function getWeather(cityName, stateCode, countryCode) {
+  const location = await convertToLatLong(cityName, stateCode, countryCode);
+  console.log('LAT: ' + location.lat);
+  console.log('LONG: ' + location.long);
 
+  const weatherResponse = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${location.lat}&lon=${location.long}&appid=${key}`
+  );
+
+  const weatherData = await weatherResponse.json();
+
+  console.log(weatherData);
+}
+
+// Async call to GeoCoding API to resolve location name to lat/long
 async function convertToLatLong(cityName, stateCode, countryCode) {
   const queryInput = `${cityName},${stateCode},${countryCode}`;
   const latLong = await fetch(
@@ -12,6 +26,11 @@ async function convertToLatLong(cityName, stateCode, countryCode) {
   console.log('Geo Response: ');
   const resJson = await latLong.json();
   console.log(resJson);
+
+  const lat = resJson[0].lat;
+  const long = resJson[0].lon;
+
+  return { lat, long };
 }
 
-convertToLatLong('clarksville', 'tn', 'us');
+getWeather('clarksville', 'tn', 'us');
