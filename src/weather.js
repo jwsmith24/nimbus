3d0ee0ff2275ed25fd065385177d788d9d86164f
Jake <jsmith181997@gmail.com>
2024-05-08 21:21:40 -0400

    Add location module

--- src/weather.js
+++ src/weather.js
@@ -2,9 +2,7 @@ const key = '240bfc06d13fa4a6c479581cad040e61';
 const cityResultLimit = 5;
 
 // Main call to get weather.
-export async function getWeather(cityName, stateCode, countryCode) {
-  const location = await convertToLatLong(cityName, stateCode, countryCode);
-
+export async function getWeather(location) {
   console.log('LAT: ' + location.lat);
   console.log('LONG: ' + location.long);
 
@@ -18,8 +16,8 @@ export async function getWeather(cityName, stateCode, countryCode) {
 }
 
 // Async call to GeoCoding API to resolve location name to lat/long
-async function convertToLatLong(cityName, stateCode, countryCode) {
-  const queryInput = `${cityName},${stateCode},${countryCode}`;
+export async function convertToLatLong(input) {
+  const queryInput = `${input.city},${input.state},${input.country}`;
   const latLong = await fetch(
     `http://api.openweathermap.org/geo/1.0/direct?q=${queryInput}&limit=${cityResultLimit}&appid=${key}`
   );
