b7ba8cc877b4dc2624feb924001c1ae87918f73c
Jake <jsmith181997@gmail.com>
2024-05-09 13:28:16 -0400

    Add toggle functionality to drop down menu

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 import { initLocationForm } from './location';
 import './style.css';
 
+const dropDownIcon = document.getElementById('drop-down');
+const menuOptions = document.querySelector('.menu-options');
+let dropDownOpen = false;
+dropDownIcon.addEventListener('click', () => {
+  toggleDropDown();
+});
+
+function toggleDropDown() {
+  dropDownOpen = dropDownOpen ? false : true;
+
+  if (dropDownOpen) {
+    menuOptions.style.display = 'none';
+  } else {
+    menuOptions.style.display = 'block';
+  }
+}
+
 initLocationForm();
