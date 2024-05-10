487649e30694d994e57b16db83ea56dc15a5a980
Jake <jsmith181997@gmail.com>
2024-05-10 08:55:58 -0400

    Move drop down diplay functionality to display module

--- src/display.js
+++ src/display.js
@@ -26,3 +26,38 @@ export function updateData(wx) {
 export function updateLocationDisplay(locationObj) {
   location.textContent = locationObj.name;
 }
+
+export function initDropDown() {
+  const dropDownIcon = document.getElementById('drop-down');
+  const menuOptions = document.querySelector('.menu-options');
+  let dropDownOpen = true;
+  dropDownIcon.addEventListener('click', () => {
+    toggleDropDown();
+  });
+
+  dropDownIcon.addEventListener('mouseover', () => {
+    displayMenu();
+  });
+
+  dropDownIcon.addEventListener('mouseout', () => {
+    hideMenu();
+  });
+
+  function toggleDropDown() {
+    dropDownOpen = dropDownOpen ? false : true;
+
+    if (dropDownOpen) {
+      hideMenu();
+    } else {
+      displayMenu();
+    }
+  }
+
+  function displayMenu() {
+    menuOptions.style.display = 'block';
+  }
+
+  function hideMenu() {
+    menuOptions.style.display = 'none';
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,37 +1,6 @@
+import { initDropDown } from './display';
 import { initLocation } from './location';
 import './style.css';
 
-const dropDownIcon = document.getElementById('drop-down');
-const menuOptions = document.querySelector('.menu-options');
-let dropDownOpen = true;
-dropDownIcon.addEventListener('click', () => {
-  toggleDropDown();
-});
-
-dropDownIcon.addEventListener('mouseover', () => {
-  displayMenu();
-});
-
-dropDownIcon.addEventListener('mouseout', () => {
-  hideMenu();
-});
-
-function toggleDropDown() {
-  dropDownOpen = dropDownOpen ? false : true;
-
-  if (dropDownOpen) {
-    hideMenu();
-  } else {
-    displayMenu();
-  }
-}
-
-function displayMenu() {
-  menuOptions.style.display = 'block';
-}
-
-function hideMenu() {
-  menuOptions.style.display = 'none';
-}
-
+initDropDown();
 initLocation();
